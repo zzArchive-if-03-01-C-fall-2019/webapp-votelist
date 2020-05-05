72873b894a5797527fccb4d338754fd5e7858f4d
Martin <s.m@hausleitner.eu>
2020-05-05 18:56:18 +0200

    Simple MongoDB Login Signup System

---
+++ App/config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://Votelist:'+ encodeURIComponent('Wy6Mm^RJ1UN#!$gy') + '@votelistcluster-vvvqw.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: dbPassword
+};
