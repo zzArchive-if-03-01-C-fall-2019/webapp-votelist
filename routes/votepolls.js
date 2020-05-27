de7a7631b03d53cd45e8acd0079137f6f5179619
byJJ <55828324+byJJ@users.noreply.github.com>
2020-05-28 00:31:14 +0200

    Votepolls can be safed at mongoDB

--- routes/votepolls.js
+++ routes/votepolls.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const Votepoll = require('./../models/votepoll')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
     res.render('votepolls/new')
 })
 
-router.post('/', (req, res) => {
-    
+router.get('/:id', async (req, res) =>{
+    const votepoll = await Votepoll.findById(req.params.id)
+    if (votepoll == null) res.redirect('/')
+    res.render('votepolls/show', { votepoll: votepoll})
+})
+router.post('/', async (req, res) => {
+    let votepoll = new Votepoll({
+        title: req.body.title,
+        description: req.body.title,
+    })
+    try{
+        votepoll = await votepoll.save()
+        res.redirect(`/votepolls/${votepoll.id}`)
+    } catch (e) {
+        res.render('votepoll/new', {votepoll: votepoll})
+    }
 })
 module.exports = router
--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose')
 const votepollRouter = require('./routes/votepolls')
 const app = express()
 
-app.set('view engine', 'ejs')
-
-app.use('/votepolls', votepollRouter)
-
-mongoose.connect('mongodb://localhost/votelist', {
+mongoose.connect('mongodb://localhost/votepoll', {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: false}))
 
 app.get('/', (req, res) => { 
     const votepolls = [{
@@ -24,5 +24,5 @@ app.get('/', (req, res) => {
     }]
     res.render('votepolls/index', {votepolls: votepolls})
 })
-
+app.use('/votepolls', votepollRouter)
 app.listen(3000)
