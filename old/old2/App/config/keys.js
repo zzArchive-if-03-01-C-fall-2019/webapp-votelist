4904e7c9bf1890c033c97db4110f8ceb77e787a1
Martin <s.m@hausleitner.eu>
2020-06-30 23:55:52 +0200

    remove old files

--- old/old2/App/config/keys.js
+++
@@ -1,5 +0,0 @@
-dbPassword = 'mongodb+srv://Votelist:'+ encodeURIComponent('Wy6Mm^RJ1UN#!$gy') + '@votelistcluster-vvvqw.mongodb.net/test?retryWrites=true';
-
-module.exports = {
-    mongoURI: dbPassword
-};
