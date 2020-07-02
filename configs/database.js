0afa1c0202c99a87375e3c2f1446c6a6245c5d32
Martin-Hausleitner <55828102+Martin-Hausleitner@users.noreply.github.com>
2020-07-02 11:29:59 +0200

    Update database.js

--- configs/database.js
+++ configs/database.js
@@ -1,3 +1,3 @@
 module.exports.config = {
-    uri: 'mongodb+srv://Votelist:votelistvotelist@votelist.vvvqw.mongodb.net/votelist?retryWrites=true&w=majority'
+    uri: 'mongodb+srv://Votelist:hidden@votelist.vvvqw.mongodb.net/votelist?retryWrites=true&w=majority'
 }
